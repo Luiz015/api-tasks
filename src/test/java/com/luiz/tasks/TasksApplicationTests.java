9b1e2d9335632bb66e14dada2e59b874917814bd
Luiz015 <ll7125875@gmail.com>
2026-07-13 19:42:44 -0300

    Commit inicial

---
+++ src/test/java/com/luiz/tasks/TasksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luiz.tasks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TasksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
