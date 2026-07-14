3b97d948bb8da386654b390112a5b68454e72e8c
Luiz015 <ll7125875@gmail.com>
2026-07-14 20:43:44 -0300

    Camada Business concluída

---
+++ src/main/java/com/luiz/tasks/business/TaskService.java
@@ -0,0 +1,70 @@
+package com.luiz.tasks.business;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Objects;
+
+
+import org.springframework.stereotype.Service;
+
+import com.luiz.tasks.business.enums.StatusTask;
+import com.luiz.tasks.infraestructure.entity.Task;
+import com.luiz.tasks.infraestructure.repository.TaskRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class TaskService {
+    
+    private final TaskRepository taskRepository;
+
+    public Task salvarTask(Task task){
+        Task tarefa = taskRepository.findByTitulo(task.getTitulo());
+
+        if (Objects.nonNull(tarefa)) {
+            throw new RuntimeException("Essa tarefa já existe");
+        }
+        return taskRepository.save(task);
+    }
+
+    public void deletarTask( String titulo){
+        Task tarefa = taskRepository.findByTitulo(titulo);
+
+        if(Objects.isNull(tarefa)){
+            throw new RuntimeException("Essa tarefa não existe");
+        }
+
+        taskRepository.delete(tarefa);
+    }
+
+    public Task buscarTaskTitulo(String titulo){
+        Task tarefa = taskRepository.findByTitulo(titulo);
+
+        if(Objects.isNull(tarefa)){
+            throw new RuntimeException("Essa tarefa não existe");
+        }
+
+        return tarefa;
+    }
+
+    public List<Task> buscarTaskStatus(StatusTask status){
+        return taskRepository.findByStatus(status);
+    }
+
+    public Task atualizarTask(String titulo, Task task){
+        Task tarefa = taskRepository.findByTitulo(titulo);
+
+        if(Objects.isNull(tarefa)){
+            throw new RuntimeException("Essa tarefa não existe");
+        }
+
+        tarefa.setTitulo(task.getTitulo());
+        tarefa.setDataAtualizacao(LocalDateTime.now());
+        tarefa.setDescricao(task.getDescricao());
+        tarefa.setStatus(task.getStatus());
+        tarefa.setDataConclusao(task.getDataConclusao());
+
+        return taskRepository.save(tarefa);
+    }
+}
