6755cb76fd47b874651bbad9645cc045df0637c5
Luiz015 <ll7125875@gmail.com>
2026-07-14 20:05:15 -0300

    Criação do método findByTitulo

--- src/main/java/com/luiz/tasks/infraestructure/repository/TaskRepository.java
+++ src/main/java/com/luiz/tasks/infraestructure/repository/TaskRepository.java
@@ -5,5 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.luiz.tasks.infraestructure.entity.Task;
 
 public interface TaskRepository extends JpaRepository<Task, Long> {
-    
+
+    Task findByTitulo(String titulo);
 }
