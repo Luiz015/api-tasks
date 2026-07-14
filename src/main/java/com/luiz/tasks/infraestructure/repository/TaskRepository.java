3c02bfcd383c31a914c7539f1d4922bde7bc0216
Luiz015 <ll7125875@gmail.com>
2026-07-14 20:44:03 -0300

    repository concluído

--- src/main/java/com/luiz/tasks/infraestructure/repository/TaskRepository.java
+++ src/main/java/com/luiz/tasks/infraestructure/repository/TaskRepository.java
@@ -1,10 +1,16 @@
 package com.luiz.tasks.infraestructure.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.luiz.tasks.business.enums.StatusTask;
 import com.luiz.tasks.infraestructure.entity.Task;
+
 
 public interface TaskRepository extends JpaRepository<Task, Long> {
 
     Task findByTitulo(String titulo);
+
+    List<Task> findByStatus(StatusTask status);
 }
